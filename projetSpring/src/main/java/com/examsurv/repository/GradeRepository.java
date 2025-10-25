68523a843af391c9541488a3c3a179dd5211857a
moallarihab114-alt <moallarihab114@gmail.com>
2025-10-25 17:45:12 +0200

    test

--- projetSpring/src/main/java/com/examsurv/repository/GradeRepository.java
+++ projetSpring/src/main/java/com/examsurv/repository/GradeRepository.java
@@ -2,5 +2,5 @@ package com.examsurv.repository;
 
 import com.examsurv.entity.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface GradeRepository extends JpaRepository<Grade, Integer> {
+public interface GradeRepository extends JpaRepository<Grade, String> {
 }
